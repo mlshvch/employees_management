c8c8be69fea8303317c668a770e98a72a5d46df3
Aliaksei Malashevich <87980319+mlshvch@users.noreply.github.com>
2022-09-20 13:03:02 +0300

    Dev/init-express (#3)
    * Install required packages
    
    * Complete project skeleton
    
    * Add build script for js-based projects
    
    * Fix code styleguide
    
    * Remove redundant js test

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express'
+<<<<<<< HEAD
 import passport from 'passport'
 import jwt from 'jsonwebtoken'
 import { Strategy as BearerStrategy } from 'passport-http-bearer'
@@ -94,3 +95,10 @@ app.use('/graphql',
 )
 
 module.exports = app.listen(port)
+=======
+const app: Express = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+>>>>>>> 90ce96b (Dev/init-express (#3))
