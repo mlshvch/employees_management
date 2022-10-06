e9c8031916a5bb87ee0b04f1202bc7f6e6581b15
mlshvch <malashevichdev@gmail.com>
2022-10-06 21:13:42 +0300

    Update server endpoints

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express'
 import { graphqlHTTP } from 'express-graphql'
-import schemaParams = require('./graphql/schema')
+import schemaParams = require('./graphql/schema/schema')
+import authSchemaParams = require('./graphql/schema/auth.schema')
 import { GraphQLSchema } from 'graphql'
 import dotenv = require('dotenv')
 dotenv.config()
@@ -8,13 +9,21 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT ?? 4000
 const schema: GraphQLSchema = new GraphQLSchema(schemaParams)
-
+const authSchema: GraphQLSchema = new GraphQLSchema(authSchemaParams)
 /* eslint-disable @typescript-eslint/no-misused-promises */
-app.use('/graphql', graphqlHTTP(
-  {
-    schema,
+
+app.use('/graphql/auth',
+  graphqlHTTP({
+    schema: authSchema,
     graphiql: process.env.NODE_ENV === 'development'
   })
 )
+
+app.use('/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === 'development'
+
+  }))
 
 module.exports = app.listen(port)
