578e45212ca6529473b0af82655a99c6ddbf7aff
mlshvch <malashevichdev@gmail.com>
2022-10-07 16:19:07 +0300

    Create Department relation and migrtion

---
+++ server/prisma/migrations/20221007131828_add_relation_department/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Department" (
+    "id" BIGSERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "managerId" BIGINT NOT NULL,
+
+    CONSTRAINT "Department_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Department_name_key" ON "Department"("name");
+
+-- CreateIndex
+CREATE INDEX "Department_id_idx" ON "Department" USING HASH ("id");
+
+-- CreateIndex
+CREATE INDEX "Department_managerId_idx" ON "Department" USING HASH ("managerId");
+
+-- AddForeignKey
+ALTER TABLE "Department" ADD CONSTRAINT "Department_managerId_fkey" FOREIGN KEY ("managerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
