e6e98d7963e1784b414328ed9c08d95730d49491
mlshvch <malashevichdev@gmail.com>
2022-10-09 21:36:53 +0300

    Add createNonExistingDepartment factory

--- server/test/factories/department.factory.ts
+++ server/test/factories/department.factory.ts
@@ -7,6 +7,13 @@ const selectRandomUser = async (): Promise<User> => {
   return await prisma.user.findMany().then(async (users: User[]): Promise<User> => { return users.length > 0 ? users[Math.floor(Math.random() * users.length)] : await createRandomUser() })
 }
 
+export const createNonExistingDepartment = async (): Promise<Department> => {
+  const departmentIds = await prisma.user.findMany({ select: { id: true } })
+  const invalidDepartment = await createRandomDepartment()
+  invalidDepartment.id = BigInt(Number(departmentIds[departmentIds.length - 1].id) + Math.round(Math.random() * 1_000_000))
+  return invalidDepartment
+}
+
 export const createRandomDepartmentData = async (managerId: number = 0): Promise<{ name: string, managerId: number | bigint, description: string }> => {
   return {
     name: faker.company.name(),
