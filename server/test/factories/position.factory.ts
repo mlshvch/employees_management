aa9c6b200f794abd13d8738eff9bcb9ba8c8ce7b
mlshvch <malashevichdev@gmail.com>
2022-10-10 12:25:16 +0300

    Add position factory

---
+++ server/test/factories/position.factory.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+import { Position, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export const createRandomPositionData = (name: string = faker.name.jobTitle(), id?: bigint | number): { id?: bigint | number, name: string } => {
+  return { id, name }
+}
+
+export const createRandomPosition = async (name: string = faker.name.jobTitle()): Promise<Position> => {
+  const positionNames = await prisma.position.findMany({ select: { name: true } })
+
+  do {
+    name = faker.name.jobTitle()
+  } while (positionNames.includes({ name }))
+
+  return await prisma.position.create({ data: { name } })
+}
+
+export const createInvalidPositionData = async (): Promise<{ id?: bigint | number, name: string }> => {
+  const invalidId = await prisma.position.findMany({ select: { id: true } })
+    .then((idList: Array<{ id: bigint }>) => { return Math.floor(Number(idList[idList.length - 1].id) + Math.floor(Math.random() * 1_000_000)) })
+
+  return createRandomPositionData(faker.name.jobTitle(), invalidId)
+}
