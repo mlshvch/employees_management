173eb14893202d50a007bf2ff1664f44e38de39e
mlshvch <malashevichdev@gmail.com>
2022-10-22 00:40:45 +0300

    Fix lint

--- server/scripts/create_log_file.ts
+++ server/scripts/create_log_file.ts
@@ -3,7 +3,7 @@ import { logFileDir, logFilePath } from '../src/logger'
 
 const main = async (): Promise<void> => {
   await fs.mkdir(logFileDir, (err) => {
-      if (err != null) console.log(err)
+    if (err != null) console.log(err)
   })
   await fs.open(logFilePath, 'a', (err) => {
     if (err != null) throw err
