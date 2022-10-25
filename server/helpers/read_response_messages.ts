a94043e15a0a4da6f29e9773e839250752fd542c
mlshvch <malashevichdev@gmail.com>
2022-10-26 02:26:13 +0300

    Authorize and validate updateDepartment mutation

--- server/helpers/read_response_messages.ts
+++ server/helpers/read_response_messages.ts
@@ -11,6 +11,7 @@ export interface ResponseMessages {
       'blankName': string
       'invalidManager': string
       'notExist': string
+      'nameAlreadyTaken': string
     }
   }
   'employee': {
