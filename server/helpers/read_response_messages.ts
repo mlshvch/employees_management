20e9ccd81916559f101d396a7a3ac74279666fc6
mlshvch <malashevichdev@gmail.com>
2022-10-25 10:12:58 +0300

    Create ResponseMessages type

--- server/helpers/read_response_messages.ts
+++ server/helpers/read_response_messages.ts
@@ -1,6 +1,33 @@
 import path from 'path'
 import * as fsPromises from 'fs/promises'
 
+export interface ResponseMessages {
+  "common": {
+    "forbidden": string
+  },
+  "department": {
+    "error": {
+      "blankName": string,
+      "invalidManager": string
+    }
+  },
+  "employee": {
+    "error": {
+      "invalidUser": string
+      "userAlreadyTaken": string,
+      "invalidPosition": string,
+      "invalidDepartment": string
+    }
+  },
+  "position": {
+    "error": {
+      "blankName": string,
+      "nameAlreadyTaken": string
+    }
+  }
+}
+
+
 export const readResponseMessages = async (): Promise<any> => {
   const filePath = path.join(process.cwd(), 'src', 'response_messages.json')
   return JSON.parse(await fsPromises.readFile(filePath, { encoding: 'utf-8' }).catch((err) => { throw err }))
